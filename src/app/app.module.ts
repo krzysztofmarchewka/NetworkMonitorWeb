ff45d058749156b275ab4f1a296a9657cb8c5168
kriskod <krzysm95@gmail.com>
2020-05-03 13:03:56 +0200

    pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { ReverseDnsService } from './services/reverse-dns.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ReverseDnsService } from "./services/reverse-dns.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AccordionModule } from 'primeng/accordion';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { CarouselModule } from 'primeng/carousel';
-import { ChartModule } from 'primeng/chart';
-import { HttpClientModule } from '@angular/common/http';
-import { KeyFilterModule } from 'primeng/keyfilter';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MonitorComponent } from './monitor/monitor.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HistoryComponent } from './history/history.component';
-import { SettingsComponent } from './settings/settings.component';
-import { DbNetworkmonitorService } from './services/db-networkmonitor.service';
-import * as FusionCharts from 'fusioncharts';
-import * as TimeSeries from 'fusioncharts/fusioncharts.timeseries';
-import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-import { FusionChartsModule } from 'angular-fusioncharts';
-import { DateFormatPipe } from './pipes/date-format.pipe';
+import { NgxPaginationModule } from "ngx-pagination";
+import { DropdownModule } from "primeng/dropdown";
+import { AccordionModule } from "primeng/accordion";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TabMenuModule } from "primeng/tabmenu";
+import { CarouselModule } from "primeng/carousel";
+import { ChartModule } from "primeng/chart";
+import { HttpClientModule } from "@angular/common/http";
+import { KeyFilterModule } from "primeng/keyfilter";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { MonitorComponent } from "./monitor/monitor.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HistoryComponent } from "./history/history.component";
+import { SettingsComponent } from "./settings/settings.component";
+import { DbNetworkmonitorService } from "./services/db-networkmonitor.service";
+import * as FusionCharts from "fusioncharts";
+import * as TimeSeries from "fusioncharts/fusioncharts.timeseries";
+import * as FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import { FusionChartsModule } from "angular-fusioncharts";
+import { DateFormatPipe } from "./pipes/date-format.pipe";
 import { NgApexchartsModule } from "ng-apexcharts";
-import { ReversednsComponent } from './reversedns/reversedns.component';
+import { ReversednsComponent } from "./reversedns/reversedns.component";
 
 FusionChartsModule.fcRoot(FusionCharts, TimeSeries, FusionTheme);
 
@@ -36,7 +38,7 @@ FusionChartsModule.fcRoot(FusionCharts, TimeSeries, FusionTheme);
     HistoryComponent,
     SettingsComponent,
     DateFormatPipe,
-    ReversednsComponent
+    ReversednsComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,9 +51,15 @@ FusionChartsModule.fcRoot(FusionCharts, TimeSeries, FusionTheme);
     KeyFilterModule,
     HttpClientModule,
     FusionChartsModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    NgxPaginationModule,
   ],
-  providers: [HttpClientModule, DbNetworkmonitorService, DateFormatPipe, ReverseDnsService],
-  bootstrap: [AppComponent]
+  providers: [
+    HttpClientModule,
+    DbNetworkmonitorService,
+    DateFormatPipe,
+    ReverseDnsService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -1,14 +1,15 @@
-import { NetworkSummary } from './../models/NetworkSummary.model';
-import { NetworkMonitor } from './../models/NetworkMonitor.model';
-import { Component, OnInit } from '@angular/core';
-import { DbNetworkmonitorService } from '../services/db-networkmonitor.service';
-import * as FusionCharts from 'fusioncharts';
-import { DateFormatPipe } from '../pipes/date-format.pipe';
+import { NetworkSummary } from "./../models/NetworkSummary.model";
+import { NetworkMonitor } from "./../models/NetworkMonitor.model";
+import { Component, OnInit } from "@angular/core";
+import { DbNetworkmonitorService } from "../services/db-networkmonitor.service";
+import * as FusionCharts from "fusioncharts";
+import { DateFormatPipe } from "../pipes/date-format.pipe";
+import { NgxPaginationModule } from "ngx-pagination";
 
 @Component({
-  selector: 'app-monitor',
-  templateUrl: './monitor.component.html',
-  styleUrls: ['./monitor.component.css'],
+  selector: "app-monitor",
+  templateUrl: "./monitor.component.html",
+  styleUrls: ["./monitor.component.css"],
 })
 export class MonitorComponent implements OnInit {
   ntData: NetworkMonitor[] = [];
@@ -16,6 +17,7 @@ export class MonitorComponent implements OnInit {
   dataLoaded = false;
   data: any;
   data2: any;
+
   responsiveOptions;
   x: Date[];
   y: any[];
@@ -30,127 +32,133 @@ export class MonitorComponent implements OnInit {
   width: string;
   height: string;
 
-  constructor(private networkData: DbNetworkmonitorService,
-    private _dateFormatPipe: DateFormatPipe) {
+  config: any;
+
+  constructor(
+    private networkData: DbNetworkmonitorService,
+    private _dateFormatPipe: DateFormatPipe
+  ) {
     this.responsiveOptions = [
       {
-        breakpoint: '1024px',
+        breakpoint: "1024px",
         numVisible: 3,
-        numScroll: 3
+        numScroll: 3,
       },
       {
-        breakpoint: '768px',
+        breakpoint: "768px",
         numVisible: 2,
-        numScroll: 2
+        numScroll: 2,
       },
       {
-        breakpoint: '560px',
+        breakpoint: "560px",
         numVisible: 1,
-        numScroll: 1
-      }
+        numScroll: 1,
+      },
     ];
 
     this.data2 = {
-      labels: ['A', 'B', 'C'],
+      labels: ["A", "B", "C"],
       datasets: [
         {
           data: [300, 50, 100],
-          backgroundColor: [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56'
-          ],
-          hoverBackgroundColor: [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56'
-          ]
-        }]
+          backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        },
+      ],
     };
 
-    this.type = 'timeseries';
-    this.width = '100%';
-    this.height = '400';
+    this.type = "timeseries";
+    this.width = "100%";
+    this.height = "400";
 
     // This is the dataSource of the chart
     this.dataSource = {
       caption: {
-        text: 'Packet analysis'
+        text: "Packet analysis",
       },
       chart: {
-        theme: 'fusion'
+        theme: "fusion",
       },
       subcaption: {
-        text: 'Initial time interval spread is from 1-Feb-2019 to 1-Dec-2021'
+        text: "Initial time interval spread is from 1-Feb-2019 to 1-Dec-2021",
       },
       data: null,
       yaxis: [
         {
           plot: {
-            value: 'Grocery Sales Value'
+            value: "Grocery Sales Value",
           },
           format: {
-            prefix: '$'
+            prefix: "$",
           },
-          title: 'Sale Value'
-        }
+          title: "Sale Value",
+        },
       ],
       xaxis: {
         initialinterval: {
           from: "1-Feb-2019 00:00:00",
-          to: "1-Dec-2021 00:00:00"
+          to: "1-Dec-2021 00:00:00",
         },
-      }
+      },
+    };
+    this.config = {
+      itemsPerPage: 10,
+      currentPage: 1,
+      totalItems: this.dataSummary.length,
     };
   }
 
+  pageChanged(event) {
+    this.config.currentPage = event;
+  }
 
   ngOnInit() {
     this.getMonitorData();
   }
 
   aggregateData() {
-
     for (let data of this.ntData) {
-      let dataFiltered = this.ntData.filter(x => x.ip_dst === data.ip_dst)
-      if (this.dataSummary.find(x => x.ip === data.ip_dst) === undefined) {
+      let dataFiltered = this.ntData.filter((x) => x.ip_dst === data.ip_dst);
+      if (this.dataSummary.find((x) => x.ip === data.ip_dst) === undefined) {
         let temp = new NetworkSummary();
         temp.ip = data.ip_dst;
-        dataFiltered.forEach(x => {
+        dataFiltered.forEach((x) => {
           temp.packetsSent += x.packets;
           temp.bytesSent += x.bytes;
           this.bytesSum += x.bytes;
           this.packetsSum += x.packets;
-        })
+        });
         this.dataSummary.push(temp);
       }
     }
-    this.dataSummary.sort((a,b) => b.bytesSent - a.bytesSent);
+    this.dataSummary.sort((a, b) => b.bytesSent - a.bytesSent);
     this.dataLoaded = true;
   }
 
   getMonitorData() {
-    this.networkData.getAllData().subscribe(nt_data => {
-
+    this.networkData.getAllData().subscribe((nt_data) => {
       this.ntData = nt_data;
-      this.x = nt_data.map(x => x.stamp_inserted as Date);
-      this.y = nt_data.map(x => x.packets as number);
+      this.x = nt_data.map((x) => x.stamp_inserted as Date);
+      this.y = nt_data.map((x) => x.packets as number);
 
       const data = [];
 
-      this.x.forEach(x => {
-        data.push([this._dateFormatPipe.transform(new Date(x)), this.y[this.x.indexOf(x)]]);
+      this.x.forEach((x) => {
+        data.push([
+          this._dateFormatPipe.transform(new Date(x)),
+          this.y[this.x.indexOf(x)],
+        ]);
       });
 
       const schema = [
         {
-          name: 'Time',
-          type: 'date',
-          format: '%d-%b-%y %-I:%-M:%-S'
+          name: "Time",
+          type: "date",
+          format: "%d-%b-%y %-I:%-M:%-S",
         },
         {
-          name: 'Quantity',
-          type: 'number',
+          name: "Quantity",
+          type: "number",
         },
       ];
 
@@ -166,14 +174,10 @@ export class MonitorComponent implements OnInit {
       this.chartsLoaded = true;
       this.aggregateData();
     }),
-      err => console.log(err);
+      (err) => console.log(err);
   }
-
 
   zip(arr1, arr2) {
     return arr1.map((k, i) => [k, arr2[i]]);
   }
-
-
-
 }
